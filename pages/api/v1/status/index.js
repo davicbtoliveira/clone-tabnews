4759ae14a5532fa42dd750a33d34329bd78a9ea5
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@hotmail.com>
2026-03-24 17:57:11 -0400

    DESAFIO: implementar o json do response

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,20 @@
 import database from "infra/database.js";
+import { Connection } from "pg";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SELECT Version();");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const actualConnections = await database.query(
+    "SELECT * FROM pg_stat_activity;",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: postgresVersion.rows[0].version,
+    max_connections: maxConnections.rows[0].max_connections,
+    actual_connctions: actualConnections.rowCount,
+  });
 }
 
 export default status;
