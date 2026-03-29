24dbd570cb29740e3c05b4cc38cb469d95270609
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@hotmail.com>
2026-03-29 17:59:31 -0400

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,17 +3,29 @@ import { Connection } from "pg";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const postgresVersion = await database.query("SELECT Version();");
-  const maxConnections = await database.query("SHOW max_connections;");
-  const actualConnections = await database.query(
-    "SELECT * FROM pg_stat_activity;",
-  );
+
+  const postgresVersionResult = await database.query("SHOW Server_Version;");
+  const pgVersionValue = postgresVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const activeConnectionsValue = activeConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
-    postgres_version: postgresVersion.rows[0].version,
-    max_connections: maxConnections.rows[0].max_connections,
-    actual_connctions: actualConnections.rowCount,
+    dependencies: {
+      database: {
+        version: pgVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        active_connections: parseInt(activeConnectionsValue),
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,8 +3,15 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
+
+  expect(
+    responseBody.dependencies.database.active_connections,
+  ).toBeGreaterThanOrEqual(0);
 });
